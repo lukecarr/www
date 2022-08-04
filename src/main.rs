ed471bf7a787b751ac92f50a5fd4e90279926d03
Luke Carr <luke_jcarr@pm.me>
2022-08-04 20:43:08 +0100

    feat: initial commit
    Signed-off-by: Luke Carr <me+oss@carr.sh>

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use chrono::offset::Utc;
+use std::fs;
+use std::time::Instant;
+use chrono::TimeZone;
+use sailfish::TemplateOnce;
+
+fn main() {
+    let now = Instant::now();
+
+    clean_output_dir().expect("Failed to clear output directory");
+
+    let index = IndexPage {
+        age: get_age(),
+    };
+    build_index(index).expect("Failed to build index page");
+
+    let links = LinksPage {};
+    build_links(links).expect("Failed to build links page");
+
+    println!("Compiled site successfully in {:.1}ms!", (now.elapsed().as_nanos() as f64) / 1e6);
+}
+
+fn get_age() -> u8 {
+    let utc = Utc;
+    let dob = utc.ymd(2001, 7, 30);
+    let age = Utc::today().signed_duration_since(dob).num_days() as f64 / 365.25;
+    return age as u8;
+}
+
+fn clean_output_dir() -> std::io::Result<()> {
+    match fs::remove_dir_all("out") {
+        Ok(_) => fs::create_dir_all("out"),
+        Err(e) => Err(e)
+    }
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "index.stpl")]
+struct IndexPage {
+    age: u8
+}
+
+fn build_index(ctx: IndexPage) -> std::io::Result<()> {
+    fs::write("out/index.html", ctx.render_once().unwrap())
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "links.stpl")]
+struct LinksPage {
+}
+
+fn build_links(ctx: LinksPage) -> std::io::Result<()> {
+    fs::write("out/links.html", ctx.render_once().unwrap())
+}
