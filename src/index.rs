dcad431ebf1f6b032daa5dbf12b854194ce9b4da
Luke Carr <me+oss@carr.sh>
2025-09-21 21:07:52 +0100

    chore: removed unused code for generating age
    Signed-off-by: Luke Carr <me+oss@carr.sh>

--- src/index.rs
+++ src/index.rs
@@ -3,16 +3,6 @@ use std::fs;
 use chrono::{TimeZone, Utc};
 use sailfish::TemplateSimple;
 
-fn get_age() -> u8 {
-    let utc = Utc;
-    let dob = utc
-        .with_ymd_and_hms(2001, 7, 30, 0, 0, 0)
-        .single()
-        .expect("Failed to parse birthday");
-    let age = Utc::now().signed_duration_since(dob).num_days() as f64 / 365.25;
-    age as u8
-}
-
 #[derive(TemplateSimple)]
 #[template(path = "index.stpl")]
 struct IndexPage {
@@ -25,7 +15,6 @@ pub fn build_index() -> std::io::Result<()> {
     let ctx = IndexPage {
         title: "Luke Carr",
         build_time: Utc::now().to_rfc3339(),
-        age: get_age(),
     };
     fs::write("out/index.html", ctx.render_once().unwrap())
 }
